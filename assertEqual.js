c4d8ae3d257d41bc661db29b0fa743e04ed83739
Sarah <sarahejohnson89@gmail.com>
2021-09-13 20:50:44 -0700

    use to write test cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
-  if (actual === expected) {
-    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+  if (`${actual}` === `${expected}`) {
+    console.log("🟢 Assertion Passed: " + `${actual}` + " === " + `${expected}`);
 
-  } else if (actual !== expected) {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+  } else if (`${actual}` !== `${expected}`) {
+    console.log("🛑 Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
   }
 };
 
