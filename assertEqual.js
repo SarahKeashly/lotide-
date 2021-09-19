f17811d992f5542f660290edcb8710e833c0d549
Sarah <sarahejohnson89@gmail.com>
2021-09-18 17:17:11 -0700

    After looking at this in the EQ Object assignment, it was not working correctly. Updated order of if/else and it is working again

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,15 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
 
-  if (`${actual}` === `${expected}`) {
-    console.log("🟢 Assertion Passed: " + `${actual}` + " === " + `${expected}`);
+  } else if (actual === expected) {
+    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
 
-  } else if (`${actual}` !== `${expected}`) {
-    console.log("🛑 Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
   }
+  // } else {
+
+  // }
 };
 
 // TEST CODE
