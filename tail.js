805bcd1125932eaf5553dc0886a8bcda0f3a3ec8
Sarah <sarahejohnson89@gmail.com>
2021-09-20 13:54:04 -0700

    added module.export= tail on line 12 and removed assertEqual testing that was previously here

--- tail.js
+++ tail.js
@@ -9,20 +9,5 @@ const tail = function(array1, array2) {
 
 };
 
-const assertEqual = function(actual, expected) {
+module.exports = tail;
 
-  if (`${actual}` === `${expected}`) {
-    console.log("🟢 Assertion Passed: " + `${actual}` + " === " + `${expected}`);
-
-  } else if (`${actual}` !== `${expected}`) {
-    console.log("🛑 Assertion Failed: " + `${actual}` + " !== " + `${expected}`);
-  }
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
