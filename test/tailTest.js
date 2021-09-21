e4b239d305bbed2061a314e012ec29a6fe701e44
Sarah <sarahejohnson89@gmail.com>
2021-09-20 18:44:04 -0700

    used Mocha and Chai to re-make tests.  Removed original testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,36 @@
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+
+// //TEST
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-//TEST
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+    assert.deepEqual(tail(['1', '2', '3']), ['2', '3']);
+  });
+
+
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []);
+
+  });
+
+  it("[returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs',]), ['Lighthouse', 'Labs']);
+
+  });
+
+
+});
